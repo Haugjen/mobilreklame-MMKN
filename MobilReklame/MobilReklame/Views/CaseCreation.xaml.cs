72b238e20ee940d61004ef742c468fc181b4234b
Mikkel <Mikk793a@edu.easj.dk>
2017-12-08 11:21:35 +0100

    test 15

--- MobilReklame/MobilReklame/Views/CaseCreation.xaml.cs
+++ MobilReklame/MobilReklame/Views/CaseCreation.xaml.cs
@@ -13,28 +13,18 @@ using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 
-// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
 
-namespace MobilReklame
+namespace MobilReklame.Views
 {
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class MainPage : Page
+    public sealed partial class CaseCreation : Page
     {
-        public MainPage()
+        public CaseCreation()
         {
             this.InitializeComponent();
-        }
-
-        private void CaseCreation(object sender, RoutedEventArgs e)
-        {
-            this.Frame.Navigate(typeof(Views.CaseCreation), null);
-        }
-
-        private void CustomerList(object sender, RoutedEventArgs e)
-        {
-            this.Frame.Navigate(typeof(Views.Customer_List), null);
         }
     }
 }
