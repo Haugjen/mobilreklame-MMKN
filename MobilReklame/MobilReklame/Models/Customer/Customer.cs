b90f74afdfe884c83506c1242a54484345d533ad
cybon <kimon5@hotmail.dk>
2017-12-12 11:25:26 +0100

    sxfsdf

--- MobilReklame/MobilReklame/Models/Customer/Customer.cs
+++ MobilReklame/MobilReklame/Models/Customer/Customer.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace MobilReklame.Order
 {
-    class Customer : IKey<string>
+    public class Customer : IKey<string>
     {
         #region Instance field
         private string _customerName;
