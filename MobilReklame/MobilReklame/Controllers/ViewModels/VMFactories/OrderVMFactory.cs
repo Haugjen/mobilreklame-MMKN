d0d1e34c5714d73bfc92bf3b7efe74faff3df89a
Mikkel <Mikk793a@edu.easj.dk>
2017-12-15 15:26:45 +0100

    bob1234

--- MobilReklame/MobilReklame/Controllers/ViewModels/VMFactories/OrderVMFactory.cs
+++ MobilReklame/MobilReklame/Controllers/ViewModels/VMFactories/OrderVMFactory.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using MobilReklame;
+using MobilReklame.Controllers.ViewModels.OrderVM;
 
 namespace MobilReklame.Controllers.ViewModels.VMFactories
 {
