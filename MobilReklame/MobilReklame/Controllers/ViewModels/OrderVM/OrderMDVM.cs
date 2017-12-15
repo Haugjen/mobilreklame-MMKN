b37f9f3c6368849892fbb51189bd93de6db9021f
Mikkel <Mikk793a@edu.easj.dk>
2017-12-15 10:09:47 +0100

    test 222222

--- MobilReklame/MobilReklame/Controllers/ViewModels/OrderVM/OrderMDVM.cs
+++ MobilReklame/MobilReklame/Controllers/ViewModels/OrderVM/OrderMDVM.cs
@@ -17,12 +17,12 @@ namespace MobilReklame.Controllers.ViewModels.OrderVM
 
         }
 
-        public int OrderNumber { get => Obj.OrderNumber; set => Obj.OrderNumber = value; }
-        public string StartupDate { get => Obj.StartupDate; set => Obj.StartupDate = value; }
-        public string DeadlineDate { get => Obj.DeadlineDate; set => Obj.DeadlineDate = value; }
-        public string WorkDescription { get => Obj.WorkDescription; set => Obj.WorkDescription = value; }
-        public OrderMaterials OrderMaterials { get => Obj.OrderMaterials; set => Obj.OrderMaterials = value; }
-        public Customer OrderCustomer { get => Obj.OrderCustomer; set => Obj.OrderCustomer = value; }
+        public int OrderNumber { get => DataPackage.OrderNumber; set => DataPackage.OrderNumber = value; }
+        public string StartupDate { get => DataPackage.StartupDate; set => DataPackage.StartupDate = value; }
+        public string DeadlineDate { get => DataPackage.DeadlineDate; set => DataPackage.DeadlineDate = value; }
+        public string WorkDescription { get => DataPackage.WorkDescription; set => DataPackage.WorkDescription = value; }
+        public OrderMaterials OrderMaterials { get => DataPackage.OrderMaterials; set => DataPackage.OrderMaterials = value; }
+        public Customer OrderCustomer { get => DataPackage.OrderCustomer; set => DataPackage.OrderCustomer = value; }
         public int Key { get; set; }
     }
 }
